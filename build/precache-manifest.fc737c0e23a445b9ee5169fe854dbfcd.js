e814d7b6ebeb239c9a501fd2421090f3ae1f6f05
Mateusz Bieliński <mateusz100022@gmail.com>
2020-05-23 19:56:57 +0200

    commit

---
+++ build/precache-manifest.fc737c0e23a445b9ee5169fe854dbfcd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5088b46c242bd5cafee3ae52fc9abb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b82cd240d6a9030f7ed",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "12df150b7760bf4f218a",
+    "url": "/static/css/main.7c2cf4b9.chunk.css"
+  },
+  {
+    "revision": "4b82cd240d6a9030f7ed",
+    "url": "/static/js/2.24e0fb36.chunk.js"
+  },
+  {
+    "revision": "778b84052f444f557713e52c2e1a7d00",
+    "url": "/static/js/2.24e0fb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12df150b7760bf4f218a",
+    "url": "/static/js/main.522f104e.chunk.js"
+  },
+  {
+    "revision": "e2117c2493713434f539",
+    "url": "/static/js/runtime-main.0cc9397a.js"
+  },
+  {
+    "revision": "dae5447e7099838af1fa0af40740f588",
+    "url": "/static/media/Lato-Black.dae5447e.woff"
+  },
+  {
+    "revision": "e29067446336b1e58be582e29387da02",
+    "url": "/static/media/Lato-Black.e2906744.woff2"
+  },
+  {
+    "revision": "6cad9641aeefc2a158298d84fc207bc3",
+    "url": "/static/media/Lato-BlackItalic.6cad9641.woff"
+  },
+  {
+    "revision": "fd9d30edbf1a083f598b697e23d4e8b0",
+    "url": "/static/media/Lato-BlackItalic.fd9d30ed.woff2"
+  },
+  {
+    "revision": "12a234e87f37d441adc9bb2e3dec13de",
+    "url": "/static/media/Lato-Bold.12a234e8.woff2"
+  },
+  {
+    "revision": "2032f2b161a7a109a9d0cc3f6ad3b735",
+    "url": "/static/media/Lato-Bold.2032f2b1.woff"
+  },
+  {
+    "revision": "7b7cc85b033ce1db351e6c2466f632b7",
+    "url": "/static/media/Lato-BoldItalic.7b7cc85b.woff2"
+  },
+  {
+    "revision": "7e15a416aef23a90c23f99f3ce99f931",
+    "url": "/static/media/Lato-BoldItalic.7e15a416.woff"
+  },
+  {
+    "revision": "4b5df23bf72e7e174aa9bc70c3ac80e2",
+    "url": "/static/media/Lato-Hairline.4b5df23b.woff2"
+  },
+  {
+    "revision": "f05daf8b55b3800a43620110dbe99926",
+    "url": "/static/media/Lato-Hairline.f05daf8b.woff"
+  },
+  {
+    "revision": "3220823f977c63226ae99c670a47f478",
+    "url": "/static/media/Lato-HairlineItalic.3220823f.woff"
+  },
+  {
+    "revision": "d3db3ad826af93b653b8863298b0dd58",
+    "url": "/static/media/Lato-HairlineItalic.d3db3ad8.woff2"
+  },
+  {
+    "revision": "90d8ae9e97ee1e2e749d4eb1d3f8bb33",
+    "url": "/static/media/Lato-Italic.90d8ae9e.woff"
+  },
+  {
+    "revision": "ba676cbaf74d3de20fee293293fc8cff",
+    "url": "/static/media/Lato-Italic.ba676cba.woff2"
+  },
+  {
+    "revision": "09201a1cc16b4ea1d1b7c8fdf90d5a60",
+    "url": "/static/media/Lato-Light.09201a1c.woff2"
+  },
+  {
+    "revision": "4ac54dd4f4c6e2a86e21aee167e1a5a1",
+    "url": "/static/media/Lato-Light.4ac54dd4.woff"
+  },
+  {
+    "revision": "158c06177a976bdd53dc56d4e14b024d",
+    "url": "/static/media/Lato-LightItalic.158c0617.woff"
+  },
+  {
+    "revision": "69e635f94393f4f294cd9df68c66335f",
+    "url": "/static/media/Lato-LightItalic.69e635f9.woff2"
+  },
+  {
+    "revision": "69279aef7fbc11101022a9f06079bbbf",
+    "url": "/static/media/Lato-Regular.69279aef.woff2"
+  },
+  {
+    "revision": "e87e43c57f21dd637791c6bfa534c1af",
+    "url": "/static/media/Lato-Regular.e87e43c5.woff"
+  }
+]);
